12eb3f5d403bf358d5d0fb239e34e7629d9485fe
Anwar Husain <a8691258@gmail.com>
2023-08-18 13:14:30 +0530

    ADD - Dashboard UI

--- app/src/main/java/com/dogimagegenerator/app/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/dogimagegenerator/app/ui/dashboard/DashboardActivity.kt
@@ -1,11 +1,12 @@
-package com.dogimagegenerator.app
+package com.dogimagegenerator.app.ui.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dogimagegenerator.app.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
